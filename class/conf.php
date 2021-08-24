d7e7ecc8090d10a78f0ed52a7de64bf95863f557
otaviobs <tavio_saraiva@hotmail.com>
2021-08-24 11:54:07 -0300

    Update conf.php

--- class/conf.php
+++ class/conf.php
@@ -5,31 +5,22 @@ $s->mailU->mail = 'naoresponder@campanhaibbl.com.br';
 $s->mailU->pass = 'DUPq7HU3NmUeRJF!';
 $s->mailU->name = 'Campanha IBBL';
 
-$s->mailC->mail = 'naoresponder@campanhaibbl.com.br';
+$s->mailC->mail = 'naoresponder@chabeberifa.com.br';
 
 //$s->mailC->name = 'Não Responder';
 
 if ( file_exists( dirname( __FILE__ ) . '/conf-local.php' ) ) {
  	include( dirname( __FILE__ ) . '/conf-local.php' );
 } else {
-	switch(1){
+	switch(0){
 		case 0:
-			$s->dom = 'https://www.ibbl.com.br';
+			$s->dom = 'https://www.chabeberifa.com.br';
 			$s->dir = '/';
 			$s->dirAdmin = $s->dir.'admin/';
-			$s->db->host = '200.234.217.146';
-			$s->db->base = 'ibbl_01';
-			$s->db->user = 'ibbl_01';
-			$s->db->pass = 'X4_X!DwRaxkh_n';
-		break;
-		case 1:
-			$s->dom = 'https://www.campanhaibbl.com.br';
-			$s->dir = '/';
-			$s->dirAdmin = $s->dir.'admin/';
-			$s->db->host = '200.234.217.146';
-			$s->db->base = 'ibbl_01';
-			$s->db->user = 'ibbl_01';
-			$s->db->pass = 'X4_X!DwRaxkh_n';
+			$s->db->host = '186.202.152.146';
+			$s->db->base = 'chabebe';
+			$s->db->user = 'chabebe';
+			$s->db->pass = 'Ch@bebeRifa098';
 		break;
 	}
 }
