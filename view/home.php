4adad4ad71ecb7cf4ccfeaa4ce5f5b6d223c09e8
Otávio Pessoal <otavio.basiliosaraiva@gmail.com>
2022-03-30 21:04:22 -0300

    Add metadados para as redes sociais e o recado inicial

--- view/home.php
+++ view/home.php
@@ -19,6 +19,31 @@
   </div>
 </div>
 
+
+<div class="modal" id="aviso" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Chá de Fralda da Heloa</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body text-justify">
+                <strong>Este site é um chá de fralda como uma rifa, pensamos em um jeito mais fácil para todos. Agradecemos desde já por sua ajuda.</strong><br /><br />
+                Vamos lá, clique para fechar essa mensagem e irá aparecer uma caixinha onde precisa colocar o seu <strong>nome completo</strong> e caso queira receber os números por e-mail, preencha o campo de e-mail também. Depois é só clicar no botão "Continuar".<br /><br />
+                Você será redirecionado para a página dos números. <strong>Os números que estão em vermelho não estão disponíeis.</strong> Então clique no(s) número(s) desejado(s) e irá paracer uma caixinha para confirmar a sua escolha.<br /><br />
+                Essa caixinha é para que você confirme o(s) número(s) escolhido(s), <strong>ao clicar no botão "Confirmar"</strong>, concluirá a(s) escolha(s) do(s) seu(s) número(s) e depois <strong>é só torcer para ganhar o prêmio de R$ 200,00 caso o seu número seja sorteado.</strong><br /><br />
+                <strong>Lembrando que se você colocar o seu e-mail, irá receber o(s) número(s) e o que precisará dar por e-mail também.</strong>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <?php
 Inline::a();
 ?>
@@ -38,6 +63,10 @@ Inline::a();
     load:function(_,F,f,e,o){},
   });
 })(jQuery);
+
+setTimeout(
+    $('#aviso').modal()
+    , 2000);
 </script>
 <?php
 Inline::b();
