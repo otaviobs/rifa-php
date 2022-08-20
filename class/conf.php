ab03856a155771a9ec6a028538937a4ef31bb4bc
Otávio Pessoal <otavio.basiliosaraiva@gmail.com>
2022-08-20 14:07:06 -0300

    clocando conf da locaweb para o deploy

--- class/conf.php
+++ class/conf.php
@@ -1,11 +1,11 @@
 <?php
-$s->mailU->host = 'smtp.chaheloa.com.br';
+$s->mailU->host = 'smtp.chadotheo.com.br';
 $s->mailU->port = 465; // LocaWeb
-$s->mailU->mail = 'naoresponder@chaheloa.com.br';
-$s->mailU->pass = 'ch@rifaHeloa22';
+$s->mailU->mail = 'naoresponder@chadotheo.com.br';
+$s->mailU->pass = 'ch@rifaTheo22';
 $s->mailU->name = 'Chá Rifa do Théo';
 
-$s->mailC->mail = 'naoresponder@chaheloa.com.br';
+$s->mailC->mail = 'naoresponder@chadotheo.com.br';
 
 //$s->mailC->name = 'Não Responder';
 
@@ -14,13 +14,13 @@ if ( file_exists( dirname( __FILE__ ) . '/conf-local.php' ) ) {
 } else {
 	switch(1){
 		case 1:
-			$s->dom = 'https://chaheloa.com.br';
+			$s->dom = 'https://chadotheo.com.br';
 			$s->dir = '/';
 			$s->dirAdmin = $s->dir.'admin/';
-			$s->db->host = '179.188.16.22';
-			$s->db->base = 'chabebe';
-			$s->db->user = 'chabebe';
-			$s->db->pass = 'ch@rifaHeloa22';
+			$s->db->host = '186.202.152.19';
+			$s->db->base = 'chabebetheo';
+			$s->db->user = 'chabebetheo';
+			$s->db->pass = 'ch@rifaTheo22';
 		break;
 	}
 }
